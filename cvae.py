67f39fb848a2153e47c4ba1f416efc1e5fa657e1
Claire <clairecheng88@gmail.com>
2019-03-12 23:16:43 -0400

    added cvae

---
+++ cvae.py
@@ -0,0 +1,177 @@
+import pretty_midi
+import glob
+import pickle
+
+from keras.layers import Lambda, Input, Dense
+from keras.models import Model
+from keras.losses import mse, binary_crossentropy
+from keras.utils import plot_model
+from keras import backend as K
+
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import os
+
+def get_piano_rolls():
+    pr = []
+
+    for file in glob.glob("type0/*.mid"):
+        pm = pretty_midi.PrettyMIDI(file) #takes a midi file and converts to pretty_midi
+        instr = pm.instruments #splits into list of instruments
+        for instrument in instr:
+            name = pretty_midi.program_to_instrument_name(instrument.program)
+            #print(name)
+            if name == "Acoustic Grand Piano": #only take the piano track
+                piano_roll = instrument.get_piano_roll() #get the piano roll, which is a np.ndarray
+                pr.append(piano_roll)
+
+
+    return pr #list of piano rolls (np.ndarray matrices)
+
+
+def sampling(args):
+    """Reparameterization trick by sampling from an isotropic unit Gaussian.
+    # Arguments
+        args (tensor): mean and log of variance of Q(z|X)
+    # Returns
+        z (tensor): sampled latent vector
+    """
+
+    z_mean, z_log_var = args
+    batch = K.shape(z_mean)[0]
+    dim = K.int_shape(z_mean)[1]
+    # by default, random_normal has mean = 0 and std = 1.0
+    epsilon = K.random_normal(shape=(batch, dim))
+    return z_mean + K.exp(0.5 * z_log_var) * epsilon
+
+
+def prepare_sequences(pr):
+
+    #need to implement this
+
+    return (net_in, net_out)
+
+
+
+def train():
+    pr = get_piano_rolls()
+    net_in, net_out = prepare_sequences(pr)
+
+    #building models
+    (x_train, y_train), (x_test, y_test) = #?????????
+    #xtrain and ytrain are the same thing, xtest and ytest are the same thing
+    #different "cuts" of the sequences, train and test should overlap by a bit
+
+    image_size = x_train.shape[1] #how to change this
+
+    #not sure if we need these four lines/how to alter for audio vs mnist data?
+    x_train = np.reshape(x_train, [-1, image_size, image_size, 1])
+    x_test = np.reshape(x_test, [-1, image_size, image_size, 1])
+    x_train = x_train.astype('float32') / 255
+    x_test = x_test.astype('float32') / 255
+
+    # network parameters
+    input_shape = #change
+    batch_size = 128
+    kernel_size = 3
+    filters = 16
+    latent_dim = 2
+    epochs = 30
+
+    # VAE model = encoder + decoder
+    # build encoder model
+    inputs = Input(shape=input_shape, name='encoder_input')
+    x = inputs
+    for i in range(2):
+        filters *= 2
+        x = Conv2D(filters=filters,
+                   kernel_size=kernel_size,
+                   activation='relu',
+                   strides=2,
+                   padding='same')(x)
+
+    # shape info needed to build decoder model
+    shape = K.int_shape(x)
+
+    # generate latent vector Q(z|X)
+    x = Flatten()(x)
+    x = Dense(16, activation='relu')(x)
+    z_mean = Dense(latent_dim, name='z_mean')(x)
+    z_log_var = Dense(latent_dim, name='z_log_var')(x)
+
+    # use reparameterization trick to push the sampling out as input
+    # note that "output_shape" isn't necessary with the TensorFlow backend
+    z = Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
+
+    # instantiate encoder model
+    encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
+    encoder.summary()
+
+    # build decoder model
+    latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
+    x = Dense(shape[1] * shape[2] * shape[3], activation='relu')(latent_inputs)
+    x = Reshape((shape[1], shape[2], shape[3]))(x)
+
+    for i in range(2):
+        x = Conv2DTranspose(filters=filters,
+                            kernel_size=kernel_size,
+                            activation='relu',
+                            strides=2,
+                            padding='same')(x)
+        filters //= 2
+
+    outputs = Conv2DTranspose(filters=1,
+                              kernel_size=kernel_size,
+                              activation='sigmoid',
+                              padding='same',
+                              name='decoder_output')(x)
+
+    # instantiate decoder model
+    decoder = Model(latent_inputs, outputs, name='decoder')
+    decoder.summary()
+
+    # instantiate VAE model
+    outputs = decoder(encoder(inputs)[2])
+    vae = Model(inputs, outputs, name='vae')
+
+    parser = argparse.ArgumentParser()
+    help_ = "Load h5 model trained weights"
+    parser.add_argument("-w", "--weights", help=help_)
+    help_ = "Use mse loss instead of binary cross entropy (default)"
+    parser.add_argument("-m", "--mse", help=help_, action='store_true')
+    args = parser.parse_args()
+    models = (encoder, decoder)
+    data = (x_test, y_test)
+
+
+
+    # VAE loss = mse_loss or xent_loss + kl_loss
+    if args.mse:
+        reconstruction_loss = mse(K.flatten(inputs), K.flatten(outputs))
+    else:
+        reconstruction_loss = binary_crossentropy(K.flatten(inputs),
+                                                  K.flatten(outputs))
+
+    reconstruction_loss *= image_size * image_size
+    kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
+    kl_loss = K.sum(kl_loss, axis=-1)
+    kl_loss *= -0.5
+    vae_loss = K.mean(reconstruction_loss + kl_loss)
+    vae.add_loss(vae_loss)
+    vae.compile(optimizer='rmsprop')
+    vae.summary()
+
+    if args.weights:
+        vae.load_weights(args.weights)
+    else:
+        # train the autoencoder
+        vae.fit(x_train,
+                epochs=epochs,
+                batch_size=batch_size,
+                validation_data=(x_test, None))
+        vae.save_weights('vae_cnn_mnist.h5')
+
+
+if __name__ == "__main__":
+    train()
--- extractingpiano.py
+++ extractingpiano.py
@@ -1,9 +1,12 @@
-from music21 import converter
 import glob
+import pretty_midi
 
-for file in glob.glob("type0/*.midi"):
-    midi = converter.parse(file)
-    print(file)
-    parts = midi.parts.stream()
-    for p in parts:
-        print(p.partName)
+for file in glob.glob("type0/*.mid"):
+    pm = pretty_midi.PrettyMIDI(file) #takes a midi file and converts to pretty_midi
+    instr = pm.instruments #splits into list of instruments
+    for instrument in instr:
+        name = pretty_midi.program_to_instrument_name(instrument.program)
+        #print(name)
+        if name == "Acoustic Grand Piano": #only take the piano track
+            piano_roll = instrument.get_piano_roll() #get the piano roll, which is a np.ndarray
+            print(piano_roll)
